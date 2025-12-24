cd9b6adb9e477bdda9960b552b77433cad56dd77
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-12-24 16:28:34 +0100

    Refactored current user's profile into a special component

---
+++ frontend/src/components/CurrentUsersProfile.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import ProfileHeader from './ProfileHeader.vue';  
+    import Card from 'primevue/card';
+    import Calendar from './Calendar.vue';
+</script>
+
+
+<template>
+    <div class="flex flex-col items-center w-[95%] h-full">
+        <Card class="w-full mt-[2vh]">
+            <template #content>
+                <ProfileHeader />
+            </template>
+        </Card>
+
+        <Calendar class="w-full mt-[2vh]"/>
+    </div>
+</template>
+
+
+<style scoped>
+</style>
--- frontend/src/components/SecondaryFrame.vue
+++ frontend/src/components/SecondaryFrame.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
   import { ref } from 'vue';
-  import ProfileHeader from './ProfileHeader.vue';  
-  import Card from 'primevue/card';
+  import CurrentUsersProfile from './CurrentUsersProfile.vue';
 </script>
 
 <template>
-  <div class="lg:w-[35vw]">
-    <Card>
-      <template #header>
-        <div class="flex items-center gap-3 px-6 pt-6 pb-2">
-          <h2 class="text-xl font-semibold text-dark">Profile view</h2>
-        </div>
-        <ProfileHeader />
-      </template>
-      
-    </Card>
+  <div class="flex flex-col items-center lg:w-[35vw] lg:h-full">
+    <CurrentUsersProfile />
   </div>
 </template>
 
