619934658867462154a0413b1320f79a58dc9945
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2026-01-06 13:40:57 +0100

    Added stats types
    Co-Authored-By: Lara Brečić <lara.brecic@fer.unizg.hr>

---
+++ frontend/src/types/stats-types.ts
@@ -0,0 +1,17 @@
+import SpeechesByInterestChart from "@/components/SpeechesByInterestChart.vue";
+
+export interface ShowStatsInterface {
+    refreshStats(): void;
+}
+
+export interface SpeechesByInterestChartInterface {
+    refreshStats(): void;
+}
+
+export interface UserGrowthChartInterface {
+    refreshStats(): void;
+}
+
+export interface DailySpeechActivityChartInterface {
+    refreshStats(): void;
+}
