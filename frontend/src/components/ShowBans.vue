abd520ff79724fdb633822518276d187365d7a34
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2026-01-06 13:33:06 +0100

    Fixed popover on ShowBans
    Co-Authored-By: Lara Brečić <lara.brecic@fer.unizg.hr>

--- frontend/src/components/ShowBans.vue
+++ frontend/src/components/ShowBans.vue
@@ -14,6 +14,7 @@
     const infoPopover = ref();
     const bannedByHandle = ref('');
     const banReason = ref('');
+    const selectedBanData = ref<any>(null);
     const rowHeight = ref(0);
     const paginatorHeight = ref(0);
     const tableHeaderHeight = ref(0);
@@ -39,9 +40,10 @@
     });
 
     const toggleInfoPopover = (event: Event, data: any) => {
-        infoPopover.value.toggle(event);
+        selectedBanData.value = data;
         bannedByHandle.value = data.banned_by.handle;
         banReason.value = data.ban_reason;
+        infoPopover.value.toggle(event);
     };
 
     const measureDimensions = () => {
@@ -184,43 +186,6 @@
                                             md:!w-11 md:!h-11"
                                 pt:icon:class="text-sm
                                             md:text-base" />
-                        <Popover ref="infoPopover">
-                            <div class="flex flex-col
-                                        justify-center items-start">
-
-                                <div class="font-semibold
-                                            mb-2">
-                                    Zabranu je uručio:
-                                </div>
-
-                                <div class="flex flex-row 
-                                            items-center">
-                                    <Avatar :image="slotProps.data.banned_by.profile_picture_url" 
-                                            :label="!slotProps.data.banned_by.profile_picture_url ? slotProps.data.banned_by.handle[0].toUpperCase() : ''" 
-                                            shape="circle" 
-                                            class="mr-2"
-                                            :pt="{
-                                                    root: { class: '!w-12 !h-12' },
-                                                    label: { class: 'text-xl' },
-                                                    image: { class: '!w-12 !h-12' }
-                                                }" />
-                                    <div>
-                                                @{{ bannedByHandle }}
-                                    </div>
-                                </div>
-                                <div class="border-b-3
-                                            border-gray-300
-                                            w-full
-                                            my-4">
-
-                                </div>
-
-                                <div>
-                                    <span class="font-semibold">Razlog zabrane:</span>
-                                    <p>{{ banReason }}</p>
-                                </div>
-                            </div>
-                        </Popover>
                     </div>
                 </template>
             </Column>
@@ -243,6 +208,44 @@
                 </template>
             </Column>
         </DataTable>
+
+        <Popover ref="infoPopover">
+            <div class="flex flex-col
+                        justify-center items-start">
+
+                <div class="font-semibold
+                            mb-2">
+                    Zabranu je uručio:
+                </div>
+
+                <div class="flex flex-row 
+                            items-center">
+                    <Avatar :image="selectedBanData?.banned_by?.profile_picture_url" 
+                            :label="!selectedBanData?.banned_by?.profile_picture_url ? selectedBanData?.banned_by?.handle?.[0]?.toUpperCase() : ''" 
+                            shape="circle" 
+                            class="mr-2"
+                            :pt="{
+                                    root: { class: '!w-12 !h-12' },
+                                    label: { class: 'text-xl' },
+                                    image: { class: '!w-12 !h-12' }
+                                }" />
+                    <div>
+                                @{{ bannedByHandle }}
+                    </div>
+                </div>
+
+                <div class="border-b-3
+                            border-gray-300
+                            w-full
+                            my-4">
+                </div>
+
+                <div>
+                    <span class="font-semibold">Razlog zabrane:</span>
+                    <p>{{ banReason }}</p>
+                </div>
+            </div>
+        </Popover>
     </div>
 </template>
 
