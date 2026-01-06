0499c4d73801778963a460eef1582588ad352e83
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2026-01-06 13:48:27 +0100

    Updated stats types
    Co-Authored-By: Lara Brečić <lara.brecic@fer.unizg.hr>

--- frontend/src/types/stats-types.ts
+++ frontend/src/types/stats-types.ts
@@ -1,17 +1,3 @@
-import SpeechesByInterestChart from "@/components/SpeechesByInterestChart.vue";
-
-export interface ShowStatsInterface {
-    refreshStats(): void;
-}
-
-export interface SpeechesByInterestChartInterface {
-    refreshStats(): void;
-}
-
-export interface UserGrowthChartInterface {
-    refreshStats(): void;
-}
-
-export interface DailySpeechActivityChartInterface {
+export interface RefreshStatsInterface {
     refreshStats(): void;
 }
