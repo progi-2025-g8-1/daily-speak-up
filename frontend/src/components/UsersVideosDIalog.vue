196aeeb60242fdc63b2df5d5ddbe66a998a57a92
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2026-01-12 20:13:03 +0100

    Added API call for monthly videos

--- frontend/src/components/UsersVideosDIalog.vue
+++ frontend/src/components/UsersVideosDIalog.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">  
-    import { ref } from 'vue';
+    import { onMounted, ref } from 'vue';
     import Dialog from 'primevue/dialog';
     import Button from 'primevue/button';
     import DatePicker from 'primevue/datepicker';
+    import Carousel from 'primevue/carousel';
 
     const props = defineProps<{
         showDialog: boolean,
@@ -11,17 +12,52 @@
 
     const emit = defineEmits(['update:showDialog']);
 
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8123/api/v1'
+
     const chosenMonth = ref<Date | null>(new Date());
     const month = ref<number>((new Date()).getMonth() + 1);
     const year = ref<number>(new Date().getFullYear());
+    const videos = ref<any[]>([]);
 
-    const handleDateChange = (date: Date | null) => {
+    const handleDateChange = async (date: Date | null) => {
         chosenMonth.value = date;
         month.value = date ? date.getMonth() + 1 : month.value;
         year.value = date ? date.getFullYear() : year.value;
         console.log('Odabrani mjesec', month.value);
         console.log('Odabrana godina', year.value);
+
+        if(props.user) {
+            const response = await fetch(`${API_BASE_URL}/user/${props.user.user_id}/${year.value}/${month.value}/videos`,
+            {
+                credentials: 'include'
+            }
+            );
+            if(response.ok) {
+                let data = await response.json();
+                videos.value = data.videos;
+                console.log('Dohvaćeni videozapisi korisnika:', videos.value);
+            } else {
+                console.error('Greška pri dohvaćanju videozapisa korisnika');
+            }
+        }
     };
+
+    onMounted(async () => {
+        if(props.user) {
+            const response = await fetch(`${API_BASE_URL}/users/${props.user.user_id}/${year.value}/${month.value}/videos`,
+            {
+                credentials: 'include'
+            }
+            );
+            if(response.ok) {
+                let data = await response.json();
+                videos.value = data.videos;
+                console.log('Dohvaćeni videozapisi korisnika:', videos.value);
+            } else {
+                console.error('Greška pri dohvaćanju videozapisa korisnika');
+            }
+        }
+    });
 
 </script>
 
