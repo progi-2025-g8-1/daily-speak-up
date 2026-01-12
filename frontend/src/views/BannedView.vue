860721e62fb5c2a0ed9043c18bd5166fa4e6b353
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2026-01-12 21:23:40 +0100

    Added basic BannedView

---
+++ frontend/src/views/BannedView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex flex-col justify-center items-center h-full p-4">
+        <h1 class="text-3xl font-bold mb-4">Zabranjen Vam je pristup aplikaciji!</h1>
+        <p class="text-lg">Vaš račun je trenutno zabranjen.</p>
+        <p class="text-lg"></p>
+    </div>
+</template>
+
+<style scoped>
+</style>
